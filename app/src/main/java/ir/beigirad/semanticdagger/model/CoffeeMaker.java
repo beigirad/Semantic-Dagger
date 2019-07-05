700b30b45f05c6406efadbdab898f4a707c1c588
Farhad Beigirad <f.beigirad@gmail.com>
2019-07-05 22:03:47 +0430

    add context arg to CoffeeMaker

--- app/src/main/java/ir/beigirad/semanticdagger/model/CoffeeMaker.java
+++ app/src/main/java/ir/beigirad/semanticdagger/model/CoffeeMaker.java
@@ -1,19 +1,23 @@
 package ir.beigirad.semanticdagger.model;
 
+import android.content.Context;
 import android.util.Log;
 
 public class CoffeeMaker {
     private final Heater heater;
     private final Pump pump;
+    private final Context context;
 
-    public CoffeeMaker(Heater heater, Pump pump) {
+    public CoffeeMaker(Heater heater, Pump pump, Context context) {
         this.heater = heater;
         this.pump = pump;
+        this.context = context;
     }
 
     public void brew() {
         heater.on();
         pump.pump();
+        Log.i("Log", "brew in " + context.getClass().getSimpleName());
         Log.i("Log", "[_]P coffee! [_]P ");
         heater.off();
     }
